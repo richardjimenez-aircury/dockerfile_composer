b8f88563e0bb2cd699e97a65df1956e96f495703
Richard Jimenez <richard-jimenez@aircury.com>
2024-03-20 11:05:30 +0100

    Finally manage to code the kata as desired

--- Classes/StringCalculator.php
+++ Classes/StringCalculator.php
@@ -1,52 +1,36 @@
 <?php declare(strict_types=1);
 
+use function PHPUnit\Framework\throwException;
+
 class StringCalculator
 {
 
 
+    /**
+     * @throws Exception
+     */
     public static function add(string $numbers): int
     {
         if ($numbers === '') {
             return 0;
         }
 
-        if (preg_match('[^\d]', $numbers)) {
-            $invalidSymbols=['\n', ';', ',', '/'];
-            $replaced=str_replace($invalidSymbols, ',', $numbers);
+        if (preg_match('/\D*/', $numbers)) {
+            $invalidSymbols = ["\n", ';', '/', ' ',':'];#LAS COMILLAS SIMPLES NO RECONOCEN \n COMO SALTO DE LINEA
+            $replaced = str_replace($invalidSymbols, ',', $numbers);
+
             $divided = explode(',', $replaced);
+            $withoutSpaces = array_filter($divided, fn($number) => $number !== '');
 
             #check for negatives
-            $negatives=array_filter($divided, fn($number)=> $number < 0);
+            $negatives = array_filter($withoutSpaces, fn($number) => $number < 0);
             if (count($negatives) > 0) {
-                throwException("negatives not allowed: ".implode(',', $negatives)." ");
+                throw new InvalidArgumentException(sprintf('negatives not allowed: %s', implode(',', $negatives)));
+                exit();
             }
 
             return (int)array_sum($divided);
         }
-
-//        if (preg_match('/[^\d,]/', $numbers)) {
-//            $divided = preg_split('/\D+/', $numbers);
-//            var_dump($divided);
-//            $sum = 0;
-//
-//            $negatives = [];
-//            foreach ($divided as $number) {
-//                $num = (int) $number;
-//                if($num == null){
-//                    $num = 0;
-//                }
-//                if ($num < 0) {
-//                    $negatives[] = $num;
-//                } else {
-//                    $sum += $num;
-//                }
-//            }
-//
-//            if (!empty($negatives)) {
-//                throwException("negatives not allowed: " . implode(',', $negatives));
-//            }
-//            return $sum;
-//        }
 
         return (int)$numbers;
     }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -2,7 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 
-require_once ("./Classes/StringCalculator.php");
+require_once("./Classes/StringCalculator.php");
 
 final class StringCalculatorTest extends TestCase
 {
@@ -15,34 +15,45 @@ final class StringCalculatorTest extends TestCase
 
     public function testAddMethodReturnOneDigit()
     {
-        $res=StringCalculator::add('1');
+        $res = StringCalculator::add('1');
         $this->assertSame(1, $res);
     }
 
     public function testAddMethodReturnTwoDigits()
     {
-        $res=StringCalculator::add('1,2');
+        $res = StringCalculator::add('1,2');
         $this->assertSame(3, $res);
     }
 
-    public function testAddMethodCanHandleUnknownNumberOfDigits(){
-        $res=StringCalculator::add('1,2,3,4,5');
+    public function testAddMethodCanHandleUnknownNumberOfDigits()
+    {
+        $res = StringCalculator::add('1,2,3,4,5');
         $this->assertSame(15, $res);
     }
 
-    public function testAddMethodCanHandleNewLines(){
-        $res=StringCalculator::add('1\n2,3;4\n5');
+    public function testAddMethodCanHandleNewLines()
+    {
+        $res = StringCalculator::add(<<<TEXT
+        1\n2,3;4\n5
+        TEXT);
         $this->assertSame(15, $res);
     }
 
-    public function testAddMethodCanHandleCustomDelimiter(){
-        $res=StringCalculator::add('//;\n1;2');
+    public function testAddMethodCanHandleCustomDelimiter()
+    {
+        $res = StringCalculator::add(<<<TEXT
+         //;
+         1;2
+         TEXT
+        );
         $this->assertSame(3, $res);
     }
 
-    public function testAddMethodCannotHandleNegativeNumbers(){
-        $res=StringCalculator::add('-1,2');
-        $this->assertSame(-1, $res);    
+    public function testAddMethodCannotHandleNegativeNumbers()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('negatives not allowed: -2,-4');
+        $res = StringCalculator::add('1;-2:-4;3');
     }
-    
+
 }
