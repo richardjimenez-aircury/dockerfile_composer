39eb4f4933c902e62034ea89cda75daf69815ba7
Richard Jimenez <richard-jimenez@aircury.com>
2024-03-21 11:11:08 +0100

    STEP 1|Password is 8 characters long

---
+++ tests/PasswordValidationTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require_once("./Classes/PasswordValidation.php");
+
+final class PasswordValidationTest extends TestCase{
+
+    public function testPasswordIsAtLeastEightCharacters()
+    {
+        $res = PasswordValidation::verifyPassword('12345678');
+        $this->assertSame(true.' Password is valid', $res);
+    }
+
+}
