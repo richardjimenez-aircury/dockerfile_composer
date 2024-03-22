b2865071d7d0dabfc07b28d5d0c9ac92aaad6d77
Richard Jimenez <richard-jimenez@aircury.com>
2024-03-22 10:39:32 +0100

    STEP 3.1|Refactored function input parameter

--- Classes/BarCodeScanner.php
+++ Classes/BarCodeScanner.php
@@ -3,15 +3,15 @@
 class BarCodeScanner
 {
 
-    public static function scanBarCode(int $barCode): string
+    public static function scanBarCode(string $barCode): string
     {
-        if ($barCode === 23456) {
+        if ($barCode === '23456') {
             return '$12.50';
         }
-        if ($barCode === 12345) {
+        if ($barCode === '12345') {
             return '$7.25';
         }
-        if ($barCode === 99999) {
+        if ($barCode === '99999') {
             return 'Error: barcode not Found';
         }
         return 'Not Found';
--- tests/PointOfSaleTest.php
+++ tests/PointOfSaleTest.php
@@ -7,16 +7,20 @@ final class PointOfSaleTest extends TestCase
 {
     public function testScannerIsWorking()
     {
-        $res= BarCodeScanner::scanBarCode(12345);
+        $res= BarCodeScanner::scanBarCode('12345');
         $this->assertSame('$7.25', $res);
     }
     public function testScannerShowsRightPrice(){
-        $res= BarCodeScanner::scanBarCode(23456);
+        $res= BarCodeScanner::scanBarCode('23456');
         $this->assertSame('$12.50', $res);
     }
     public function testScannerCanNotFindBarCode()
     {
-        $res= BarCodeScanner::scanBarCode(99999);
+        $res= BarCodeScanner::scanBarCode('99999');
         $this->assertSame('Error: barcode not Found', $res);
     }
+//    public function testScannerReceivesEmptyBarCode(){
+//        $res= BarCodeScanner::scanBarCode('');
+//        $this->assertSame('Error: empty barcode', $res);
+//    }
 }
